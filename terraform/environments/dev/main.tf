e083b925024c86f2483d6af9a02527847a07f04c
Bilal Syed <syedbilal228@gmail.com>
2026-02-16 23:03:17 +0000

    fix

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -189,6 +189,7 @@ module "iam_lambda" {
     module.dynamodb.user_accounts_table_arn,
     module.dynamodb.payment_links_table_arn,
     module.dynamodb.subscription_links_table_arn,
+    module.dynamodb.transactions_table_arn,
   ]
   dynamodb_index_arns = [
     "${module.dynamodb.user_identities_table_arn}/index/*",
