c98fa5205504a945ffc37b357e5b60b93a7f2b31
Bilal Syed <syedbilal228@gmail.com>
2026-02-27 23:18:33 +0000

    fix delete item

--- terraform/modules/iam_lambda/main.tf
+++ terraform/modules/iam_lambda/main.tf
@@ -28,6 +28,7 @@ data "aws_iam_policy_document" "lambda" {
     actions = [
       "dynamodb:GetItem",
       "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
       "dynamodb:BatchWriteItem",
       "dynamodb:UpdateItem",
       "dynamodb:Query",
